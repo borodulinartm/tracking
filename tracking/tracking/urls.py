58987cba57611e6f08753a18bffdd4379d9dc5e4
Artem <borodulin.artm@inbox.ru>
2022-09-10 13:15:57 +0300

    Create first test View

--- tracking/tracking/urls.py
+++ tracking/tracking/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from tracking_dev.views import *
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index)
 ]
