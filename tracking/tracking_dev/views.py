3758fa1c398cf276c6c0cd48279348e6afda5c61
Artem <borodulin.artm@inbox.ru>
2023-03-11 10:16:44 +0300

    Редактирование приоритета задачи доступно из страницы описания приоритета

--- tracking/tracking_dev/views.py
+++ tracking/tracking_dev/views.py
@@ -607,7 +607,8 @@ def priority_description(request, priority_id):
         'data_group': data,
         'show_choose_project': 1,
         'what_open': 3,
-        'list_projects': all_projects
+        'list_projects': all_projects,
+        'priority_id': priority_id
     })
 
 
@@ -2780,6 +2781,55 @@ def change_compound_projects_to_type_task(request, type_id):
                                      "type_id": type_id})
     return JsonResponse({"data": [], "all_data": pack_projects(accesible_projects),
                          "type_id": type_id})
+
+
+def change_compound_projects_to_priority(request, priority_id):
+    if not request.user.is_authenticated:
+        return HttpResponseNotFound()
+
+    if not request.user.is_staff:
+        return HttpResponseForbidden()
+
+    accesible_projects = Project.objects.raw(
+        raw_query=f"select * from tracking_dev_employee_projects tdep "
+                  f"join tracking_dev_employee tde on tdep.employee_id = tde.employee_id "
+                  f"join tracking_dev_project tdp on tdp.project_id = tdep.project_id "
+                  f"where tde.user_id = {request.user.id} and tdp.is_activate=TRUE;"
+    )
+
+    if request.method == "POST":
+        # If the type is POST, we need check if request is ajax.
+        is_ajax = request.headers.get('X-Requested-With') == 'XMLHttpRequest'
+        if is_ajax:
+            array_with_project_id = request.POST.get('array_with_project_id')
+            project_list = array_with_project_id.split(',')
+
+            if array_with_project_id != "-1":
+                try:
+                    with connection.cursor() as cursor:
+                        # 1. Firstly, we need to remove the all projects
+                        cursor.execute(f"delete from tracking_dev_priority_projects where priority_id={priority_id}")
+
+                        # 2. Secondary, add projects with the specific type id to project
+                        for project_id in project_list:
+                            cursor.execute(
+                                f"insert into tracking_dev_priority_projects(priority_id, project_id) "
+                                f"values ({priority_id}, {project_id})")
+                except Exception as error:
+                    print(error)
+
+                list_projects = Priority.objects.raw(
+                    raw_query=f"select distinct 1 as priority_id, tdp.project_id, tdp.code, tdp.\"name\", tdp.description"
+                              f", tdp.date_create from "
+                              f"tracking_dev_priority_projects tdtp join tracking_dev_project tdp "
+                              f"on tdtp.project_id = tdp.project_id where tdp.project_id in ({array_with_project_id})"
+                )
+
+                return JsonResponse({"data": pack_projects(list_projects),
+                                     "all_data": pack_projects(accesible_projects),
+                                     "priority_id": priority_id})
+    return JsonResponse({"data": [], "all_data": pack_projects(accesible_projects),
+                         "priority_id": priority_id})
 
 
 # This method adds new users to the project
